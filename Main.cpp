58f089375537e42ba3684bb55b5d4b4e0ab030a9
dylanjcassidy <dylan.j.cassidy@uconn.edu>
2023-01-16 13:26:04 -0500

    Renders EBO, press F10 for wireframe and F11 for fill mode

--- Main.cpp
+++ Main.cpp
@@ -95,9 +95,15 @@ int main()
 	glDeleteShader(fragmentShader);
 	//Triangle vertices setup
 	float vertices[] = {
-	0.0f, 0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	-0.5f, -0.5f, 0.0f
+		 0.5f,  0.5f, 0.0f,  // top right
+		 0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+
+	unsigned int indices[] = {
+	0, 1, 3,  // tri 1
+	1, 2, 3   // tri 2
 	};
 
 	//Vertex array object
@@ -114,6 +120,12 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	//Element buffer object
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	//Modify and enable vertex attribute
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
 	glEnableVertexAttribArray(0);
@@ -130,7 +142,8 @@ int main()
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		//Check/call events and swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -144,6 +157,14 @@ int main()
 
 void process_input(GLFWwindow* window)
 {
+	if (glfwGetKey(window, GLFW_KEY_F10) == GLFW_PRESS)
+	{
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	}
+	if (glfwGetKey(window, GLFW_KEY_F11) == GLFW_PRESS)
+	{
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 	{
 		glfwSetWindowShouldClose(window, true);
